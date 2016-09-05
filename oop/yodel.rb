4d2e302fcd0f77fa805cccdfc48d7f7662c66873
Nate Macaso <nathanmacaso@gmail.com>
2016-09-04 21:38:55 -0500

    Initial commit

--- oop/yodel.rb
+++ oop/yodel.rb
@@ -9,9 +9,16 @@
 # constructor accordingly.
 
 class Yodel
+  attr_reader :segment, :count
+  def initialize(yodel_hash)
+    @segment = yodel_hash[:segment]
+    @count = yodel_hash[:count]
+  end
 
+  def articulate
+    @segment * @count
+  end
 end
-
 
 # Driver code - don't touch anything below this line.
 puts "TESTING the Yodel class..."
