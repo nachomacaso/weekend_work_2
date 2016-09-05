0378afff8336146b37862e41d2a0e091ea52c038
Nate Macaso <nathanmacaso@gmail.com>
2016-09-05 12:48:43 -0500

    Updated Algo without using reverse method

--- algo/palindrome.rb
+++ algo/palindrome.rb
@@ -1,7 +1,15 @@
 # Complete the method called palindrome? which should accept a string as a parameter and return a boolean indicated whether the string is a palindrome. A palindrome is a word that reads the same both forwards and backwards. Examples: eye, madam, racecar
 
 def palindrome?(string)
-  if string == string.reverse
+  array = string.each_char.to_a
+  reverse_array = []
+  length = array.length
+  until length == -1
+    reverse_array << array[length]
+    length -= 1
+  end
+  reverse_string = reverse_array.join
+  if reverse_string == string
     result = true
   else
     result = false
